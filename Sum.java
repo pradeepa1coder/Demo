8db5e53180d7309616ee895f15b6efa65257e357
sai9133 <gsaigsai19919@gmail.com>
2023-04-22 08:33:02 +0530

    changes are updated

--- Sum.java
+++ Sum.java
@@ -2,6 +2,12 @@ class Sum
  {
 	public static int addSum(int a,int b,int c)
 	{
-		return a+b+c;
+		return (a+b+c);
 	}
+	public static void main(String[] args)
+	{
+                        System.out.println("sub of three number :- "+addSum(10,5,8));
+	}
+
+    
 }
